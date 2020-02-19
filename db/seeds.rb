1db53b2b3e7149ef5efcd343c6266579d5b999d4
Brian Heise <bnheise@gmail.com>
2020-02-19 16:19:08 +0900

    seeded the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'generating 10 flats'
+
+IMG_URLS = %w[https://images.unsplash.com/photo-1512917774080-9991f1c4c750 https://images.unsplash.com/photo-1475855581690-80accde3ae2b https://images.unsplash.com/photo-1518780664697-55e3ad937233 https://images.unsplash.com/photo-1527030280862-64139fba04ca https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=668&q=80].freeze
+
+index = 0
+5.times do
+  Flat.create!(
+    name: "#{Faker::Address.community} #{Faker::Cannabis.strain}",
+    address: Faker::Address.street_address,
+    description: (Faker::Lorem.sentences(number: (1..5).to_a.sample, supplemental: true)).join(" "),
+    price_per_night: (50..100).to_a.sample + 0.99,
+    number_of_guests: (1..10).to_a.sample,
+    picture_url: IMG_URLS[index]
+  )
+  index += 1
+end
