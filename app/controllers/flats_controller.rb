455639d550500a619960ed4ef9763ce8e980bc89
Brian Heise <bnheise@gmail.com>
2020-02-19 18:28:14 +0900

    made index page. working on show page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,33 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
+  end
 end
